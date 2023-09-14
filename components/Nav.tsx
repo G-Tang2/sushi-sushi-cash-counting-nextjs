fdaac7e9dfea36aa9082d9689d95b8727e914c79
Garvin Tang <garvin9006@gmail.com>
2023-09-14 19:37:49 +1000

    added bottom border in nav bar

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,5 +1,13 @@
 "use client";
 
+import { signOut } from "next-auth/react";
+
 export default function Home() {
-  return <nav className="flex justify-end"></nav>;
+  return (
+    <nav className="flex justify-end px-8 py-3 border-b-[1px] border-gray-500 border-opacity-60">
+      <button className="btn w-20" onClick={() => signOut()}>
+        Sign Out{" "}
+      </button>
+    </nav>
+  );
 }
