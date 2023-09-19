08ebb46b543bf9891a5f3545f29baf37f1d1ce3e
Garvin Tang <garvin9006@gmail.com>
2023-09-19 19:26:41 +1000

    clicking sign out button redirects user to sign in page

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -5,8 +5,11 @@ import { signOut } from "next-auth/react";
 export default function Home() {
   return (
     <nav className="flex justify-end px-8 py-3 border-b-[1px] border-gray-500 border-opacity-60">
-      <button className="btn w-20" onClick={() => signOut()}>
-        Sign Out{" "}
+      <button
+        className="btn w-20"
+        onClick={() => signOut({ callbackUrl: "/" })}
+      >
+        Sign Out
       </button>
     </nav>
   );
