eab64c38e495124638873ae5eed05f12d7f69992
Garvin Tang <garvin9006@gmail.com>
2023-09-20 16:05:35 +1000

    added title to the login page

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,11 @@ import LoginForm from "@/components/(form)/LoginForm";
 
 export default function SignIn() {
   return (
-    <div className="flex flex-col h-screen justify-center items-center">
+    <div className="flex flex-col h-screen items-center">
+      <div className="pt-24 pb-12">
+        <h1 className="text-4xl font-bold">Cash Counting</h1>
+        <h3 className="my-5 font-light">For Sushi Sushi</h3>
+      </div>
       <LoginForm />
     </div>
   );
