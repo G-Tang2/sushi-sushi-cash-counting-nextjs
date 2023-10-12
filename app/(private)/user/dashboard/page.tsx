00f3cb5ae93f1e4b5b752c239a78df601d430bd7
Garvin Tang <garvin9006@gmail.com>
2023-10-12 15:34:26 +1100

    added button for creating daily taking

--- app/(private)/user/dashboard/page.tsx
+++ app/(private)/user/dashboard/page.tsx
@@ -1,5 +1,12 @@
+import Link from "next/link";
 import React from "react";
 
 export default function Home() {
-  return <div>User Dashboard</div>;
+  return (
+    <div>
+      <Link className="btn w-fit" href="create/daily-taking">
+        Create New
+      </Link>
+    </div>
+  );
 }
